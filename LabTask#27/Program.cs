1e2037be260675a5bde9f5e4fe83731db13227ab
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-04-26 13:24:27 +0400

    LabTask#27 added

---
+++ LabTask#27/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] arr = { "Ali", "Murad", "Pesha", "Nicat", "Qadir" };
+
+head:
+Console.WriteLine("Zehmet olmasa bir herf daxil edin: ");
+string input = Console.ReadLine();
+bool checkChar = char.TryParse(input, out char letter);
+if (!checkChar) goto head;
+
+Console.WriteLine("------------------------------");
+ void  ArrayWords(char ch)
+{
+    int location = 0;
+    foreach (var item in arr)
+    {
+        for (int i = 0; i < item.Length; i++)
+        {
+            if (item[i] == ch)
+            {
+                location = i + 1;
+                Console.WriteLine($"{item} - {ch} herfinin yeri :  {location}");
+            }
+          
+        }
+    }
+}
+
+ArrayWords(letter);
+
